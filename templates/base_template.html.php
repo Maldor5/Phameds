f775036981f460ce0d2bb790e25e648f20445890
Malcolm Kananura <mwine360@gmail.com>
2023-06-01 18:49:25 +0300

    added comments

--- templates/base_template.html.php
+++ templates/base_template.html.php
@@ -1,88 +1,88 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Inventory</title>
-    <link rel="stylesheet" href="/css/dashboard.css">
-  </head>
-  <body>
-    <div class="container">
-    <div class="sidebar">
-        <div class="top">
-          <div class="logo">
-            <img src="/assets/icons/drupal.png">
-            <h2>phameds</h2>
-          </div>
-        </div>
-        <a href="#" class="active">
-          <img src="assets/icons/dashboard.png">
-          <h3>dashboard</h3>
-        </a>
-        <a href="./Sales.php" class="sales active" onclick="selected1();">
-          <img src="/assets/icons/plus.png">
-          <h3>Sales</h3>
-        </a>
-        <a href="./Inventory.php" class="inventory" onclick="selected();">
-          <img src="/assets/icons/inventory.png">
-          <h3>Inventory</h3>
-        </a>
-        <a href="./staff.php" class="staff">
-          <img src="/assets/icons/staff icon.png">
-          <h3>Staff</h3>
-        </a>
-        <!-- <a href="#" class="reports" onclick="selected2();">
-          <img src="icons/medicine.png">
-          <h3>Reports</h3>
-        </a> -->
-        <a href="./logout.php">
-          <img src="/assets/icons/enter.png">
-          <h3>Log out</h3>
-        </a>         
-      </div>
-      
-      <!-- ---------END OF SIDEBAR----------------- -->
+      <head>
+        <title>Inventory</title>
+        <link rel="stylesheet" href="/css/dashboard.css">
+      </head>
+      <body>
+             <div class="container">
+                    <div class="sidebar">
+                          <div class="top">
+                                <div class="logo">
+                                      <img src="/assets/icons/drupal.png">
+                                      <h2>phameds</h2>
+                                </div>
+                          </div>
+                          <a href="#" class="active">
+                                <img src="assets/icons/dashboard.png">
+                                <h3>dashboard</h3>
+                          </a>
+                          <a href="./Sales.php" class="sales active" onclick="selected1();">
+                                <img src="/assets/icons/plus.png">
+                                <h3>Sales</h3>
+                          </a>
+                          <a href="./Inventory.php" class="inventory" onclick="selected();">
+                                <img src="/assets/icons/inventory.png">
+                                <h3>Inventory</h3>
+                          </a>
+                          <a href="./staff.php" class="staff">
+                                <img src="/assets/icons/staff icon.png">
+                                <h3>Staff</h3>
+                          </a>
+                          <!-- <a href="#" class="reports" onclick="selected2();">
+                            <img src="icons/medicine.png">
+                            <h3>Reports</h3>
+                          </a> -->
+                          <a href="./logout.php">
+                                <img src="/assets/icons/enter.png">
+                                <h3>Log out</h3>
+                          </a>         
+                    </div>
+          
+          <!-- ---------END OF SIDEBAR----------------- -->
 
-      <main>
-        <div class="main-top">
-              <div>
-                  <h1>Dashboard</h1>
-              </div>
-              <div class="right-top">
-                    <div class="theme-toggler">
-                          <button class="button1 active" onclick="changeTheme1();">
-                                  <img class="toggle" src="/assets/icons/moon.png">
-                          </button>
-                          <button class="button2" onclick="changeTheme();">
-                                  <img class="toggle" src="/assets/icons/brightness.png">
-                          </button>
-                    </div>
-                    <div class="profile">
-                          <div>
-                              <p><h5><?=$name?></h5></p>
-                              <small class="text-muted">Admin</small>
-                          </div>
-                          <div>
-                                <img src="pictures/124-1246857_employee-avatar-transparent-background-png-icone-chef-d.png">
-                          </div>
-                    </div>
-              </div>
-         </div>
-                  <!-- -------------------end of HEADER----------------------- -->
-        <!-- <div class="date">
-          <input type="date">
-        </div> -->
-           <!-- ---------------------end of DASHBORD----------------------- -->
-        <?=$output?>
-      </main>
-    </div>
-  </body>
-  <script>
-    // changes theme
-    
-   const themeToggler = document.querySelector('.theme-toggler');
-   themeToggler.addEventListener('click', () =>{
-   document.body.classList.toggle('dark-theme-variables');
-    })
-  
-  
-  </script>
-  </html>
+                    <main>
+                            <div class="main-top">
+                                  <div>
+                                      <h1>Dashboard</h1>
+                                  </div>
+                                  <div class="right-top">
+                                        <div class="theme-toggler">
+                                              <button class="button1 active" onclick="changeTheme1();">
+                                                      <img class="toggle" src="/assets/icons/moon.png">
+                                              </button>
+                                              <button class="button2" onclick="changeTheme();">
+                                                      <img class="toggle" src="/assets/icons/brightness.png">
+                                              </button>
+                                        </div>
+                                        <div class="profile">
+                                              <div>
+                                                  <p><h5><?=$name?></h5></p>
+                                                  <small class="text-muted">Admin</small>
+                                              </div>
+                                              <div>
+                                                    <img src="pictures/124-1246857_employee-avatar-transparent-background-png-icone-chef-d.png">
+                                              </div>
+                                        </div>
+                                  </div>
+                            </div>
+                                <!-- -------------------end of HEADER----------------------- -->
+                      <!-- <div class="date">
+                        <input type="date">
+                      </div> -->
+                        <!-- ---------------------end of DASHBORD----------------------- -->
+                      <?=$output?>
+                    </main>
+               </div>
+      </body>
+          <script>
+                  // changes theme
+                  
+                const themeToggler = document.querySelector('.theme-toggler');
+                themeToggler.addEventListener('click', () =>{
+                document.body.classList.toggle('dark-theme-variables');
+                  })
+                
+          
+          </script>
+</html>
--- templates/staff.html.php
+++ templates/staff.html.php
@@ -1,40 +1,40 @@
 <div class="table" style="margin-top: 4rem;">
-          <h2>Staff</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Staff</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Gender</th>
-                <th>Contact</th>
-                <th>email</th>
-                <th>Role</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              
-              <tr>
-                  <?php 
-                  //displays data from staff table
-                  while($row = mysqli_fetch_assoc($result))
-                  {
-                  ?>
-                  <td><?php echo $row['id'];?></td>
-                  <td><?php echo $row['first_name'];?></td>
-                  <td><?php echo $row['last_name'];?></td>
-                  <td><?php echo $row['gender'];?></td>
-                  <td><?php echo $row['contact'];?></td>
-                  <td><?php echo $row['email'];?></td>
-                  <td><?php echo $row['role'];?></td>
+        <h2>Staff</h2>
+        <table>
+                  <thead>
+                          <tr>
+                            <th>Staff</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Gender</th>
+                            <th>Contact</th>
+                            <th>email</th>
+                            <th>Role</th>
+                            
+                          </tr>
+                  </thead>
+                  <tbody>
+                    
+                          <tr>
+                              <?php 
+                              //displays data from staff table
+                              while($row = mysqli_fetch_assoc($result))
+                              {
+                              ?>
+                              <td><?php echo $row['id'];?></td>
+                              <td><?php echo $row['first_name'];?></td>
+                              <td><?php echo $row['last_name'];?></td>
+                              <td><?php echo $row['gender'];?></td>
+                              <td><?php echo $row['contact'];?></td>
+                              <td><?php echo $row['email'];?></td>
+                              <td><?php echo $row['role'];?></td>
 
-              </tr>
-                <?php
-                  }
+                          </tr>
+                          <?php
+                            }
 
-                  ?>
-              
-            </tbody>
-          </table>
-        </div>
+                            ?>
+                    
+                  </tbody>
+       </table>
+</div>
